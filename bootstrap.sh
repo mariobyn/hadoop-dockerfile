f4d4859c68f64ad64b74fbb1dea1a3737eca03a7
Marius Baisan <mbd@Mariuss-MacBook-Pro.local>
2015-04-26 21:30:29 +0300

    baseimage is debian now, also format namenode when running image

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,9 +8,11 @@ $HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 
 source /etc/bash_profile
 
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
+start-dfs.sh
+start-yarn.sh
 
+hadoop namenode -format
+hadoop fs -mkdir input
 
 if [[ $1 == "-d" ]]; then
   while true; do sleep 1000; done
